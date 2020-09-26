574e64ab56822924dbe84a17f5b470c82cba8e28
AlainDevis <devisalain04@gmail.com>
2020-09-26 15:33:00 +0200

    Function declaration in js

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,12 @@
-var birthdayDate= document.getElementsByName("BD");
+function dayOftheWeek(){
+    var birthdayDate = document.getElementById("BD").value;
+    var genderMF;
+    if (document.getElementById("male").checked = true){
+        genderMF = "male";
+    }
+    if (document.getElementById("female").checked = true){
+        genderMF = "female";
+    }  
+    // alert(birthdayDate);
+    document.getElementById("Result").innerHTML = birthdayDate + " " + genderMF;
+}
